7ddd30802ce0fffc3181c92726aa2f9a30b7a02e
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2022-12-13 03:22:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,95 @@
-class WorldSimulation:
-    def __init__(self,time = None,  morning = None):
-        self.morning = morning
-        self.time = time
+time = input("Please enter the time: ")
+x,y = time.split(":")
+current_time = x
+cur = y
 
-    def checkDay(self):
-        time = input("Please enter the time: ")
-        self.time = time
-        x,y = time.split(":")
-        if 8 <= int(x) <= 18 and 0 <= int(y) <= 59:
-            self.morning = True
-            print("It's morning... ")
-        elif 19 <= int(x) <= 24 or 1 <= int(x) <= 7 and 00 <= int(y) <= 59:
-            self.morning = False
-            print("It's night zzz... ")
-        else:
-            print("INVALIDINPUT")
+class Day:
+    def __init__(self):
+        pass
+    def morning(self):
+        if 19 >= int(current_time) >= 8:
+            return True
+        return False
 
+class Frog:
+    def __init__(self):
+        pass
+    
     def Status(self):
-        if self.morning == True:
-            print("Frog is awake...")
-            
-        else:
-            print("Frog is sleeping...")
-    
-    def Eat(self):
-        if self.morning == False:
-            print("Frog ate and fell asleep.")
-        elif self.morning == True:
-            x,y = self.time.split(":")
-            if int(x) % 2 == 0:
-                print("Frog is eatting grass..")
-            else:
-                print("Frog is chilling :D")
-    
-    def Move(self):
-        if self.morning == False:
-            print("Let's assume that our frog is not sleepwalker,so at the moment is asleep and not moving:")
-        else:
-            print("Frog enjoys life in this wonderful world, (briefly moving)")
+        if day.morning():
+            return("Frog is awake...")
+        return ("Frog is sleeping...")
     
     def Breath(self):
-        if self.morning == True:
-            print("Frog is breating cuz its morning:")
-        elif self.morning == False:
-            print("Frog is not breathing. But don't worry he is alive:")
-    
-    def Tree(self):
-        if self.morning == True:
-            print("As it is morning, the sun causes photosynthesis to occur.")
-        elif self.morning == False:
-            print("it's night, so no more photosynthesis.")
-    
-    def Grass(self):
-        if self.morning == False:
-            print("Grass is unlimited.")
-        elif self.morning == True:
-            x,y = self.time.split(":")
-            if int(x) % 2 == 0:
-                print("The grass grows endlessly.")
-            else:
-                print("Grass is unlimited, the frog can eat whenever he wants")
-    def Sun(self):
-        print("In this world the sun shines for 10 hours and does not shine for 14 hours")
-        x,y = self.time.split(":")
-        if self.morning == True:
-            x,y = self.time.split(":")
-            minutes = (16 - int(x))*60 + int(y)
-            print(f"The sun will shine for another {minutes} minutes")
+        if day.morning():
+            return("Frog is breating cuz its morning:")
         else:
-            print("Its night, so the sun does not shine")
-
-        
+            return("Frog is not breathing. But don't worry he is alive:")
     
-day = WorldSimulation()
-day.checkDay()
-day.Tree()
-day.Status()
-day.Eat()
-day.Grass()
-day.Move()
-day.Breath()
-day.Sun()
+    def Eat(self):
+        if not day.morning():
+            return("Frog ate and fell asleep.")
+        elif day.morning():
+            if int(current_time) % 2 == 0:
+                return("Frog is eatting grass..")
+            else:
+                return("Frog is chilling :D")
+    
+    def Move(self):
+        if not day.morning():
+            return("Let's assume that our frog is not sleepwalker,so at the moment is asleep and not moving:")
+        else:
+            return("Frog enjoys life in this wonderful world, (briefly moving)")
+
+
+class Tree:
+    def __init__(self):
+        pass
+    
+    def tree_(self):
+        if day.morning():
+            return("As it is morning, the sun causes photosynthesis to occur.")
+        else:
+            return("it's night, so no more photosynthesis.")
+
+class Grass:
+    def __init__(self):
+        pass
+    
+    def grass_(self):
+        if not day.morning():
+            print("Grass is unlimited.")
+        elif day.morning():
+            if int(current_time) % 2 == 0:
+                return("The grass grows endlessly.")
+            else:
+                return("Grass is unlimited, the frog can eat whenever he wants")
+
+class Sun:
+    def __init__(self):
+        pass
+    def sun_(self):
+        # In this world the sun shines for 10 hours and does not shine for 14 hours
+        if day.morning():
+            minutes = (16 - int(current_time))*60 + int(cur)
+            return(f"The sun will shine for another {minutes} minutes")
+        else:
+            return("Its night, so the sun does not shine")
+    
+
+
+day = Day()
+tree = Tree()  
+frog = Frog()
+sun = Sun()
+grass = Grass()
+
+
+
+print(frog.Status())
+print(frog.Eat())
+print(frog.Breath())
+print(frog.Move())
+print(tree.tree_())
+print(sun.sun_())
+print(grass.grass_())
